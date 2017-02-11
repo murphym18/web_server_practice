f07fc6151fbf538e0a74f62fef3a4426a86c17fd
Michael Murphy <murphym18@gmail.com>
2017-02-11 02:38:28 -0800

    Read and write to the socket

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::net::{TcpListener, TcpStream, SocketAddr};
-use std::io::Write;
+use std::io::{Read, Write};
+
+const REQUEST_BUFFER_SIZE: usize = 8192;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
@@ -9,15 +11,36 @@ fn main() {
                 on_connection(stream, addr);
             }
             Err(e) => {
-
+                println!("Error accepting connection: {}", e);
             }
         }
-
     }
 }
 
 fn on_connection(mut stream: TcpStream, sock_addr: SocketAddr) {
-    let out = String::from("hello!!!!! MOO!!!\n");
+    let out = String::from("hi");
+    let mut buffer: Vec<u8> = Vec::with_capacity(REQUEST_BUFFER_SIZE);
+    buffer.resize(REQUEST_BUFFER_SIZE, 0);
     println!("got new connection from {}", sock_addr.ip());
-    stream.write(out.as_bytes());
+    match stream.read(&mut buffer[..]) {
+        Ok(n) => {
+            println!("Read {} bytes from socket", n);
+            buffer.truncate(n);
+            let buf_str = String::from_utf8(buffer).unwrap();
+            for 
+            println!("{}", buf_str);
+        }
+        Err(e) => {
+            println!("Error reading socket: {}", e);
+        }
+    }
+
+    match stream.write(out.as_bytes()) {
+        Ok(n) => {
+            println!("wrote {} bytes", n);
+        }
+        Err(e) => {
+            println!("Error writing to socket: {}", e);
+        }
+    };
 }
