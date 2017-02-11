219a79742d6bef1dd5cecd07db961fcba496d6ad
Michael Murphy <murphym18@gmail.com>
2017-02-11 03:27:08 -0800

    replace pattern matching code with calls to unwrap

--- src/main.rs
+++ src/main.rs
@@ -6,41 +6,23 @@ const REQUEST_BUFFER_SIZE: usize = 8192;
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
     loop {
-        match listener.accept() {
-            Ok((stream, addr)) => {
-                on_connection(stream, addr);
-            }
-            Err(e) => {
-                println!("Error accepting connection: {}", e);
-            }
-        }
+        let (stream, addr) =  listener.accept().unwrap();
+        on_connection(stream, addr);
     }
 }
 
 fn on_connection(mut stream: TcpStream, sock_addr: SocketAddr) {
-    let out = String::from("hi");
+    let out = String::from("<!DOCTYPE HTML><body><h1>Hi</h1></body>");
     let mut buffer: Vec<u8> = Vec::with_capacity(REQUEST_BUFFER_SIZE);
     buffer.resize(REQUEST_BUFFER_SIZE, 0);
-    println!("got new connection from {}", sock_addr.ip());
-    match stream.read(&mut buffer[..]) {
-        Ok(n) => {
-            println!("Read {} bytes from socket", n);
-            buffer.truncate(n);
-            let buf_str = String::from_utf8(buffer).unwrap();
-            for 
-            println!("{}", buf_str);
-        }
-        Err(e) => {
-            println!("Error reading socket: {}", e);
-        }
-    }
 
-    match stream.write(out.as_bytes()) {
-        Ok(n) => {
-            println!("wrote {} bytes", n);
-        }
-        Err(e) => {
-            println!("Error writing to socket: {}", e);
-        }
-    };
+    println!("got new connection from {}", sock_addr.ip());
+    let n = stream.read(&mut buffer[..]).unwrap();
+    println!("Read {} bytes from socket", n);
+    buffer.truncate(n);
+    let buf_str = String::from_utf8(buffer).unwrap();
+    println!("{}", buf_str);
+
+    let n = stream.write(out.as_bytes()).unwrap();
+    println!("wrote {} bytes", n);
 }
