05fd32b921906ed6826cfb00df5cd4f9613a3cc5
Michael Murphy <murphym18@gmail.com>
2017-02-11 05:06:18 -0800

    Kinda works as an http server (its hacked together). ¯\_(ツ)_/¯

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::net::{TcpListener, TcpStream, SocketAddr};
 use std::io::{Read, Write};
+use std::path::Path;
+use std::env;
+use std::fs::File;
 
 const REQUEST_BUFFER_SIZE: usize = 8192;
 
 fn main() {
+    let p = env::current_dir().unwrap();
+    println!("The current directory is {}", p.display());
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
     loop {
         let (stream, addr) =  listener.accept().unwrap();
@@ -12,23 +17,51 @@ fn main() {
 }
 
 fn on_connection(mut stream: TcpStream, sock_addr: SocketAddr) {
-    let out = String::from("<!DOCTYPE HTML><body><h1>Hi</h1></body>");
+
     let mut buffer: Vec<u8> = Vec::with_capacity(REQUEST_BUFFER_SIZE);
     buffer.resize(REQUEST_BUFFER_SIZE, 0);
 
+    // get request data
     println!("got new connection from {}", sock_addr.ip());
     let n = stream.read(&mut buffer[..]).unwrap();
     println!("Read {} bytes from socket", n);
     buffer.truncate(n);
-    let buf_str = String::from_utf8(buffer).unwrap();
-    let v: Vec<&str> = buf_str.split("\r\n").collect();
-    let get_line = &v[0];
-    let tmp: Vec<&str> = get_line.split(" ").collect();
-    let method_name: &str = tmp[0];
-    let path: &str = tmp[1];
 
-    println!("method = {}, path = {}", method_name, path);
+    // parse path from request data
+    let mut path_string = String::new();
+    find_path(buffer, &mut path_string);
 
+    // make a path from path String
+    let req_path = Path::new(&path_string);
+    let path = if req_path.exists() {
+        req_path
+    } else {
+        Path::new("404.html")
+    };
+
+    // println!("path is {}", path.display());
+
+    // load file from path
+    let mut fd = File::open(path).unwrap();
+    let mut file_data = String::new();
+    let r = fd.read_to_string(&mut file_data).unwrap();
+    println!("read to string result {}", r);
+
+    // add headers
+    let mut out = String::from("HTTP/1.0 200 OK\r\n\r\n");
+    out.push_str(file_data.as_str());
+
+    // send file back
     let n = stream.write(out.as_bytes()).unwrap();
     println!("wrote {} bytes", n);
+}
+
+fn find_path(buffer: Vec<u8>, path_out: &mut String) {
+    let req = String::from_utf8(buffer).unwrap();
+    let v: Vec<&str> = req.split("\r\n").collect();
+    let get_line = &v[0];
+    let tmp: Vec<&str> = get_line.split(" ").collect();
+    let path: &str = tmp[1];
+    path_out.push_str(path);
+    path_out.remove(0);
 }
